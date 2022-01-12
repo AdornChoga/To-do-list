fdbe18fbca7d81e9be6605271506341d36faed72
asanchy <sanchyclips@gmail.com>
2022-01-12 17:17:24 +0200

    Create module and write code for fetching and updating LS data

---
+++ src/local_data.js
@@ -0,0 +1,9 @@
+class tasksData {
+  static fetchData() {
+    return JSON.parse(localStorage.getItem('tasks'))
+  }
+
+  static setData(data) {
+    localStorage.setItem('tasks', JSON.stringify(data));
+  }
+}
