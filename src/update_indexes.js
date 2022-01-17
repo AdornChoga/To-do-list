715f33fcce90aaecf72bd1f6b4007dd5dd32e03f
asanchy <sanchyclips@gmail.com>
2022-01-17 10:21:12 +0200

    Remove development folders and files

--- src/update_indexes.js
+++
@@ -1,9 +0,0 @@
-import tasksData from './local_data.js';
-
-export default function updateIndexes() {
-  const tasks = tasksData.fetchData();
-  tasks.forEach((task) => {
-    task.id = tasks.indexOf(task);
-    tasksData.setData(tasks);
-  });
-}
