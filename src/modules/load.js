a18f0c039bc85f71709512cfc4265e5c1ba9e88f
asanchy <sanchyclips@gmail.com>
2022-02-23 08:40:49 +0200

    Modularize code by separating concerns

--- src/modules/load.js
+++ src/modules/load.js
@@ -11,7 +11,7 @@ function loadTasks() {
   for (let i = 0; i < tasks.length; i += 1) {
     listContainer.innerHTML += `
     <li class="task-item">
-      <input type="checkbox" name="task" value="task1" class="to-do" 
+      <input type="checkbox" name="task" value="task1" class="to-do"
       ${tasks[i].completed === true ? 'checked' : ''}>
       <span class="task-description">${tasks[i].description}</span>
       <i class="fas fa-ellipsis-v option"></i>
